1921aea7629d5b59fe34586796b711bbb36608c7
mstick26 <mstickcortes26@gmail.com>
2024-07-05 10:11:07 -0500

    contenedor preguntas y respuestas terminadas

---
+++ src/pages/QA.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './assets/css/pricing.css'
+
+function QA() {
+    return (
+        <div>
+  <h1>shfihdfijsiojviokjfiojbh</h1>
+        <section className='seccion_Q&A'>
+
+
+        </section>
+        </div>
+      
+    )
+
+}
+export default QA;
